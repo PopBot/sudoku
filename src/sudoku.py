f82ac732eb2aa7badf35ce34ec6fc8674f70e0de
Jarod Wong <jarodjaw@gmail.com>
2022-04-13 19:25:45 -0700

    More meat and bones.

--- src/sudoku.py
+++ src/sudoku.py
@@ -1,20 +1,51 @@
-def generate_empty_sudoku_board():
+import math
+
+
+def generate_empty_sudoku_board(size=9):
     """
     Generate an empty sudoku board.
     """
-    board = [[0 for i in range(9)] for j in range(9)]
+    if not check_if_perfect_square(size):
+        raise Exception("The size of the board must be a perfect square.")
+    board = [[0 for i in range(size)] for j in range(size)]
     return board
 
 
-def is_valid_number(x, y, board):
+def is_valid_number(x, y, board, size=9):
     """
     Check if the number is valid in the board.
     """
-    for i in range(9):
+    for i in range(size):
         if board[x][i] == board[y][i]:
             return False
-    for i in range(9):
-        if board[x][i] == board[x//3*3 + y//3][i]:
+    for i in range(size):
+        if board[x][i] == board[x//get_square_root(size)*get_square_root(size) + y//get_square_root(size)][i]:
             return False
     return True
+
+
+def is_valid_board(board, size=9):
+    """
+    Check if the board is valid.
+    """
+    for i in range(get_square_root(size)):
+        for j in range(get_square_root(size)):
+            if board[i][j] != 0:
+                if not is_valid_number(i, j, board):
+                    return False
+    return True
+
+
+def check_if_perfect_square(n):
+    """
+    Check if the number is a perfect square.
+    """
+    return int(math.sqrt(n))**2 == n
+
+
+def get_square_root(n):
+    """
+    Get the square root of the number.
+    """
+    return int(math.sqrt(n))
 
