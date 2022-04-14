429b92b844696b924354e01c922ce816bb09fb56
Jarod Wong <jarodjaw@gmail.com>
2022-04-13 19:38:18 -0700

    About to make boards.

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ if __name__ == '__main__':
     board = generate_empty_sudoku_board()
     print(board[3][4])
     print(generate_empty_sudoku_board())
+    print(get_possible_numbers(3, 4, board))
--- src/sudoku.py
+++ src/sudoku.py
@@ -49,3 +49,55 @@ def get_square_root(n):
     """
     return int(math.sqrt(n))
 
+
+def get_possible_numbers(x, y, board, size=9):
+    """
+    Get the possible numbers for the board programmatically.
+    In the future will try to use memoization to store numbers already placed.
+    """
+    numbers = [i for i in range(1, size+1)]
+    for i in range(size):
+        # Check the row
+        if board[x][i] in numbers:
+            numbers.remove(board[x][i])
+        # Check the column
+        if board[i][y] in numbers:
+            numbers.remove(board[i][y])
+        # Check the square
+        if board[x//get_square_root(size)*get_square_root(size) + y//get_square_root(size)][i] in numbers:
+            numbers.remove(board[x//get_square_root(size)*get_square_root(size) + y//get_square_root(size)][i])
+    return numbers
+
+
+def get_next_empty_cell(board, size=9):
+    """
+    Get the next empty cell.
+    """
+    for i in range(size):
+        for j in range(size):
+            if board[i][j] == 0:
+                return i, j
+    return None, None
+
+
+def solve_sudoku(board, size=9):
+    """
+    Solve the sudoku board.
+    """
+    x, y = get_next_empty_cell(board, size)
+    if x is None:
+        return True
+    for i in get_possible_numbers(x, y, board, size):
+        board[x][y] = i
+        if solve_sudoku(board, size):
+            return True
+        board[x][y] = 0
+    return False
+
+
+def generate_sudoku_board_iteration_loop(size=9):
+    board = generate_empty_sudoku_board(size)
+
+
+def generate_sudoku_board_seed_method(size=9):
+    board = generate_empty_sudoku_board(size)
